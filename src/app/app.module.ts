2bc5c3d3dfdb97be1680c02eaf63ab4fa94f06c5
Lorick <lorick@live.fr>
2019-02-15 12:13:31 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BonjourComponent } from './bonjour/bonjour.component';
+import { AvisComponent } from './avis/avis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BonjourComponent,
+    AvisComponent
   ],
   imports: [
     BrowserModule
